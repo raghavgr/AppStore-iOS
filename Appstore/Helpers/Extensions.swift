4f30974cc824b151466ffe8897097f0bb097bd8b
raghavgr94 <raghavgr94@gmail.com>
2019-05-21 13:52:00 -0700

    :art: Moved UI Extensions to Extensions.swift in Helpers group

---
+++ Appstore/Helpers/Extensions.swift
@@ -0,0 +1,33 @@
+//
+//  Extensions.swift
+//  Appstore
+//
+//  Created by Sai Grandhi on 5/21/19.
+//  Copyright © 2019 Grandhi. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    convenience init(text: String, font: UIFont) {
+        self.init(frame: .zero)
+        self.text = text
+        self.font = font
+    }
+}
+
+extension UIImageView {
+    convenience init(cornerRadius: CGFloat) {
+        self.init(image: nil)
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = true
+        self.contentMode = .scaleAspectFill
+    }
+}
+
+extension UIButton {
+    convenience init(title: String) {
+        self.init(type: .system)
+        self.setTitle(title, for: .normal)
+    }
+}
